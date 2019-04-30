6a4f007d88e9742e6ecdde6a02f04903f9e55240
elco45 <dennischeong2003@yahoo.es>
2019-04-30 00:20:51 -0400

    Removing unnecessary things for user

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,16 +13,17 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-import GlobalStyle from '../../global-styles';
+import HomeNav from '../../components/HomeNav';
 
 export default function App() {
   return (
     <div>
+      <HomeNav />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
-      <GlobalStyle />
+      {/* </div> */}
     </div>
   );
 }
