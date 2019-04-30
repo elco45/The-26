bafb165724b922abd800b896302abe017767cd48
elco45 <dennischeong2003@yahoo.es>
2019-04-30 00:22:12 -0400

    Adding global reducer

--- app/reducers.js
+++ app/reducers.js
@@ -6,6 +6,7 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 import history from 'utils/history';
+import globalReducer from 'containers/App/reducer';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 /**
@@ -14,6 +15,7 @@ import languageProviderReducer from 'containers/LanguageProvider/reducer';
 export default function createReducer(injectedReducers = {}) {
   const rootReducer = combineReducers({
     language: languageProviderReducer,
+    global: globalReducer,
     router: connectRouter(history),
     ...injectedReducers,
   });
