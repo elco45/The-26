65c3b394f44475b64b57bc086c232f4a55b3fdd8
elco45 <dennischeong2003@yahoo.es>
2019-04-30 00:21:58 -0400

    Specifying necessary packages for firebase

--- app/firebase.js
+++ app/firebase.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import '@firebase/firestore';
+import 'firebase/auth';
+import 'firebase/database';
 import ReduxSagaFirebase from 'redux-saga-firebase';
 
 const myFirebaseApp = firebase.initializeApp({
@@ -10,5 +13,4 @@ const myFirebaseApp = firebase.initializeApp({
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
 });
 
-console.log(myFirebaseApp);
 export const reduxSagaFirebase = new ReduxSagaFirebase(myFirebaseApp);
