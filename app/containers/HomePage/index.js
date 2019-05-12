7d6336eea6dce3c01df1d9d743208a0f58821a65
elco45 <dennischeong2003@yahoo.es>
2019-05-12 14:38:36 -0400

    Starting home page logic

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,72 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+import {
+  makeSelectCurrentUser,
+  makeSelectSync,
+  makeSelectLoading,
+} from '../App/selectors';
+
+import ClientHomePage from '../ClientHomePage';
 import messages from './messages';
 
-export default function HomePage() {
-  return (
-    <div>
-      <FormattedMessage {...messages.welcome} />
-    </div>
-  );
+class HomePage extends React.Component {
+  renderHomePage() {
+    return (
+      <div>
+        <FormattedMessage {...messages.welcome} />
+      </div>
+    );
+  }
+
+  renderClientHomePage() {
+    return <ClientHomePage />;
+  }
+
+  renderAdminHomePage() {
+    return this.renderHomePage();
+  }
+
+  renderByUserRole() {
+    const { user } = this.props;
+    if (user && user.profile.roles.includes('admin')) {
+      return this.renderAdminHomePage();
+    }
+    if (user && user.profile.roles.includes('client')) {
+      return this.renderClientHomePage();
+    }
+    return this.renderHomePage();
+  }
+
+  render() {
+    const { syncing, loading } = this.props;
+    return syncing || loading ? <div>Loading</div> : this.renderByUserRole();
+  }
 }
+
+HomePage.propTypes = {
+  user: PropTypes.object,
+  syncing: PropTypes.bool,
+  loading: PropTypes.bool,
+};
+
+const mapDispatchToProps = {};
+
+const mapStateToProps = createStructuredSelector({
+  user: makeSelectCurrentUser(),
+  syncing: makeSelectSync(),
+  loading: makeSelectLoading(),
+});
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(withConnect)(HomePage);
